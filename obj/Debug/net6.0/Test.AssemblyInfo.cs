927859d54f0d6e312f3bd32a34409cf5ea0bbc81
Claudio Lanzetti Amador <66079114+lanzetti1@users.noreply.github.com>
2024-05-05 14:39:00 -0300

    Atualização

--- obj/Debug/net6.0/Test.AssemblyInfo.cs
+++ obj/Debug/net6.0/Test.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a65850e6ddb8c31b654100ff073c30327d40ab45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
