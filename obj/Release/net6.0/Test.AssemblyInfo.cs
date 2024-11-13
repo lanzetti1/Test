731617ee54da750057879d84550966df329d21f7
Claudio Lanzetti Amador <claudiolamador@hotmail.com>
2024-11-13 13:44:13 -0300

    Atualização de repositórios

--- obj/Release/net6.0/Test.AssemblyInfo.cs
+++ obj/Release/net6.0/Test.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da9c9f8f2c54fe4662bc04f47989a47eec95bc4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
